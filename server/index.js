1cc5f569abd0b3a45b08b785d76b1cde093448fc
Sergey Klykov <pp@rawsound.ru>
2020-06-27 14:46:03 +0300

    [server] current module state calculations fixed

--- server/index.js
+++ server/index.js
@@ -29,16 +29,67 @@ app.use(express.json());
  */
 
 /**
-  * @typedef {number[]} Time [hours, minutes]
+  * @typedef {number[]} Time — [hours, minutes]
+  * @property {number} 0 — hours
+  * @property {number} 1 — minutes
   */
 
+/**
+ * @param {number} ms — milliseconds
+ * @returns {number} minutes
+ */
 const getMinutesFromMs = ms => ((ms / (1000 * 60)) % 60);
+
+/**
+ * @param {number} ms — milliseconds
+ * @returns {number} hours
+ */
 const getHoursFromMs = ms => ((ms / (1000 * 60 * 60)) % 24);
+
+/**
+ * @param {number} ms — milliseconds
+ * @returns {Time}
+ */
 const getTimeFromMs = ms => [getHoursFromMs(ms), getMinutesFromMs(ms)];
+
+/**
+ * @param {string} string — time in format 'HH:mm'
+ * @returns {Time} time in format [h, m]
+ */
 const getTimeFromString = string => string.split(':').map(Number);
+
+/**
+ * @param {Time} time
+ * @returns {number} milliseconds
+ */
 const getMsFromTimeArray = ([hours, minutes]) => hours * 60 * 60 * 1000 + minutes * 60 * 1000;
-const getHoursAndMinutes = ([hours, minutes]) => [hours % 24, minutes % 60];
+
+/**
+ * removes the excess time from hours and minutes
+ * @param {Time} time
+ * @returns {Time}
+ */
+const extractHoursAndMinutes = ([hours, minutes]) => [hours % 24 + (minutes / 60), minutes % 60];
+
+/**
+ * @param {Time} time
+ * @returns {number}
+ */
+const extractDays = ([hours, minutes]) => (hours + minutes / 60) / 24;
+
+/**
+ * gets time between two times
+ * @param {Time} a
+ * @param {Time} b
+ */
 const getRemainTime = ([h, m], [hh, mm]) => [getRemainTimeForUnits(h, hh, 24), getRemainTimeForUnits(m, mm, 60)];
+
+/**
+ * gets time between two times
+ * @param {Time} a
+ * @param {Time} b
+ * @param {number} units — amount of units [24,60,...]
+ */
 const getRemainTimeForUnits = (a, b, units) => ((a > b) ? (getRemainTimeForUnits(a, units) + (a - b)) : b - a);
 
 /**
@@ -52,11 +103,18 @@ const getConfigEntity = (duration, onTime) => {
   const [currentHours, currentMinutes] = [currentDate.getHours(), currentDate.getMinutes()];
   const [onHours, onMinutes] = getTimeFromString(onTime);
   const [durationHours, durationMinutes] = getTimeFromMs(duration);
-  const [offHours, offMinutes] = getHoursAndMinutes([onHours + durationHours, onMinutes + durationMinutes]);
+  const offTime = [onHours + durationHours, onMinutes + durationMinutes];
+  const [offHours, offMinutes] = extractHoursAndMinutes(offTime);
+  const hasNextDaySwitching = extractDays(offTime) | 0;
 
   const isOn = (
     (onHours < currentHours || (onHours === currentHours && onMinutes <= currentMinutes)) && 
-    (offHours > currentHours || (offHours === currentHours && offMinutes > currentMinutes))
+    (
+      offHours > currentHours ||
+      (offHours === currentHours && offMinutes > currentMinutes) ||
+      (offHours < currentHours && hasNextDaySwitching) ||
+      (offHours === currentHours && offMinutes < currentMinutes && hasNextDaySwitching)
+    )
   );
 
   const timeBeforeSwitch = getRemainTime([currentHours, currentMinutes], isOn ? [offHours, offMinutes] : [onHours, onMinutes]);
