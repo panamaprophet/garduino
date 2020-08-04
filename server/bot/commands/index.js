32e02e2e29e968d38537702da96eea79a2eb90c3
Sergey Klykov <pp@rawsound.ru>
2020-08-04 11:31:33 +0200

    handling controller's configuration via telegram bot

--- server/bot/commands/index.js
+++ server/bot/commands/index.js
@@ -1,26 +1,21 @@
-const help = async ({reply}) => {
-    const response = 
-    `Greetings. These are the things i can do:
-  
-    /help — show this message
-    /now — show current params of sensors
-    /stat — show data overview
-    /setup — edit configuration`;
-  
-    reply(response);
-};
+const HELP_PLACEHOLDER = `Greetings. These are the things i can do:
+
+/help — show this message
+/now — show current params of sensors
+/stat — show data overview
+/setup — edit configuration`;
+
+
+const help = async ({reply}) => reply(HELP_PLACEHOLDER);
 
 const now = async ({scene}) => scene.enter('now');
 
-const stat = async ({reply}) => reply('statistics');
-
-const setup = async ({reply}) => reply('light schedule');
+const setup = async ({scene}) => scene.enter('setup');
 
 
 module.exports = {
-    help,
     now,
-    stat,
     setup,
+    help,
     start: help,
 };
--- server/bot/controllers/index.js
+++ server/bot/controllers/index.js
@@ -1,70 +1,60 @@
 const WizardScene = require('telegraf/scenes/wizard');
-const {getLastUpdateEventLogByControllerId} = require('../actions');
+const {
+    actionHandler,
+    ACTION_NOW,
+    ACTION_STAT,
+} = require('../actions');
 const {getInlineKeyboard} = require('../helpers');
 const {getControllerIds} = require('../../resolvers/controller');
 
 
-const ACTIONS = ['now', 'stat', 'setup'];
+const SELECT_CONTROLLER_STEP_INDEX = 0;
 
 
-const processAction = async ({db}, action, controllerId) => {
-    switch (action) {
-        case 'now':
-            return await getLastUpdateEventLogByControllerId(db, controllerId);
-        default:
-            return 'unknown action';
+const selectController = async ctx => {
+    const {db} = ctx;
+    const controllerIds = await getControllerIds(db);
+
+    ctx.reply('Select a controller', getInlineKeyboard(controllerIds));
+
+    return ctx.wizard.next();
+};
+
+const selectAction = async ctx => {
+    const {db} = ctx;
+    const selectedControllerId = ctx.update.callback_query.data;
+    const controllerIds = await getControllerIds(db);
+
+    if (!controllerIds.includes(selectedControllerId)) {
+        return ctx.wizard.selectStep(SELECT_CONTROLLER_STEP_INDEX);
     }
+
+    ctx.session.controllerId = selectedControllerId;
+
+    ctx.reply('Select an action', getInlineKeyboard([ACTION_NOW, ACTION_STAT]));
+
+    return ctx.wizard.next();
+};
+
+const handleAction = async ctx => {
+    const {db} = ctx;
+    const selectedAction = ctx.update.callback_query.data;
+    const {controllerId} = ctx.session;
+
+    if (!controllerId) {
+        return ctx.wizard.selectStep(SELECT_CONTROLLER_STEP_INDEX);
+    }
+
+    const result = await actionHandler(selectedAction, {db, controllerId});
+
+    ctx.reply(result);
+
+    return ctx.scene.leave();
 };
 
 
-const MainSceneController = new WizardScene('now',
-    // select a controller
-    async ctx => {
-        const {db} = ctx;
-        const controllerIds = await getControllerIds(db);
-
-        ctx.reply('Select a controller', getInlineKeyboard(controllerIds));
-
-        return ctx.wizard.next();
-    },
-    // select an action
-    async ctx => {
-        const {db} = ctx;
-        const selectedControllerId = ctx.update.callback_query.data;
-        const controllerIds = await getControllerIds(db);
-
-        if (!controllerIds.includes(selectedControllerId)) {
-            return ctx.wizard.back();
-        }
-
-        ctx.session.controllerId = selectedControllerId;
-
-        ctx.reply('Select an action', getInlineKeyboard(ACTIONS));
-
-        return ctx.wizard.next();
-    },
-    // run action
-    async ctx => {
-        const selectedAction = ctx.update.callback_query.data;
-        const {controllerId} = ctx.session;
-
-        if (!controllerId) {
-            return ctx.wizard.selectStep(0);
-        }
-
-        if (!ACTIONS.includes(selectedAction)) {
-            return ctx.wizard.back();
-        }
-
-        const response = await processAction(ctx, selectedAction, controllerId);
-
-        ctx.reply(response);
-
-        return ctx.scene.leave();
-    },
+module.exports = new WizardScene('now',
+    selectController,
+    selectAction,
+    handleAction,
 );
-
-
-module.exports = {
-    MainSceneController,
-};
