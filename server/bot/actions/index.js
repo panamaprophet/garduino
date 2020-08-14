523944726685f0e6132e5ff9ea09d1ed75526b93
Sergey Klykov <pp@rawsound.ru>
2020-08-14 18:21:23 +0200

    a bit more statistics
    * simple svg chart stat
    
    * 7d and 24h stat
    
    * a bit of chart helpers refactoring
    
    * a bit of more stat

--- server/bot/actions/index.js
+++ server/bot/actions/index.js
@@ -1,7 +1,25 @@
-const {getSensorDataByKey} = require('../../helpers');
-const {getLastUpdateEventLog} = require('../../resolvers/log');
+const {last} = require('ramda');
+const {format, subDays, subWeeks} = require('date-fns');
+const {getSensorDataByKey, processData} = require('../../helpers');
+const {getLastUpdateEventLog, getUpdateEventLogStat} = require('../../resolvers/log');
+const {createSvgChart, svg2png} = require('../../helpers/chart');
 
 
+const ACTION_NOW = 'main/now';
+
+const ACTION_STAT_WEEK = 'main/stat/week';
+
+const ACTION_STAT_DAY = 'main/stat/day';
+
+/**
+ * @typedef {Object} ActionResult
+ * @property {String} text
+ * @property {Buffer} [image]
+ */
+
+/**
+ * @returns {Promise<ActionResult>}
+ */
 const getLastUpdateEventLogByControllerId = async ({db, controllerId}) => {
     const eventData = await getLastUpdateEventLog(db, controllerId);
     const {payload} = eventData;
@@ -9,21 +27,49 @@ const getLastUpdateEventLogByControllerId = async ({db, controllerId}) => {
     const [temperature] = getSensorDataByKey(payload, 'temperature');
     const response = `Humidity: ${humidity.value}%, Temperature: ${temperature.value}°C`;
 
-    return response;
+    return {
+        text: response,
+    };
 };
 
+/**
+ * @returns {Promise<ActionResult>}
+ */
+const getStat = async ({db, controllerId}, dateFrom) => {
+    const data = await getUpdateEventLogStat(db, controllerId, dateFrom);
+    const {minHumidity, maxHumidity, minTemperature, maxTemperature, ...chartData} = processData(data);
+    const svgChart = createSvgChart(chartData);
+    const pngChart = await svg2png(svgChart);
 
-const ACTION_NOW = 'main/now';
+    const text = [
+        `${chartData.date[0]} — ${last(chartData.date)}`,
+        '',
+        `max humidity: ${maxHumidity.humidity}% on ${format(maxHumidity.date, 'dd.MM.yy HH:mm')}`,
+        `max temperature: ${maxTemperature.temperature}°C on ${format(maxTemperature.date, 'dd.MM.yy HH:mm')}`,
+        '',
+        `min humidity: ${minHumidity.humidity}% on ${format(minHumidity.date, 'dd.MM.yy HH:mm')}`,
+        `min temperature: ${minTemperature.temperature}°C on ${format(minTemperature.date, 'dd.MM.yy HH:mm')}`,
+    ].join('\n');
 
-const ACTION_STAT = 'main/stat';
+    return {
+        text,
+        image: pngChart,
+    };
+};
+
+const getDayStat = context => getStat(context, subDays(Date.now(), 1));
+
+const getWeekStat = context => getStat(context, subWeeks(Date.now(), 1));
 
 
 const actionHandler = async (action, context) => {
     switch (action) {
         case ACTION_NOW:
             return await getLastUpdateEventLogByControllerId(context);
-        case ACTION_STAT:
-            return 'not implemented';
+        case ACTION_STAT_WEEK:
+            return await getWeekStat(context);
+        case ACTION_STAT_DAY:
+            return await getDayStat(context);
         default:
             return 'action is not supported';
     };
@@ -32,6 +78,7 @@ const actionHandler = async (action, context) => {
 
 module.exports = {
     ACTION_NOW,
-    ACTION_STAT,
+    ACTION_STAT_WEEK,
+    ACTION_STAT_DAY,
     actionHandler,
 };
--- server/helpers/index.js
+++ server/helpers/index.js
@@ -1,3 +1,5 @@
+const {format} = require('date-fns');
+
 const getContext = request => ({
     db: request.app.locals.db,
     params: request.params,
@@ -7,8 +9,86 @@ const getContext = request => ({
 
 const getSensorDataByKey = (haystack, needle) => haystack.filter(({key}) => key === needle);
 
+const range = (from, to, step = 1) => {
+    const result = [];
+
+    for (let i = from; i <= to; i += step) {
+        result.push(i);
+    }
+
+    return result;
+};
+
+const reduceItemsCountBy = (items, limit) => {
+    if (items.length <= limit) {
+        return items;
+    }
+
+    const result = items.filter((item, index) => index % limit === 0);
+
+    return result;
+};
+
+
+const processData = data => {
+    const initData = {
+        date: [],
+        temperature: [],
+        humidity: [],
+        maxHumidity: data[0],
+        minHumidity: data[0],
+        minTemperature: data[0],
+        maxTemperature: data[0],
+    };
+
+    return data.reduce((result, item, index) => {
+        if (result.maxHumidity.humidity < item.humidity) {
+            result.maxHumidity = item;
+        }
+
+        if (result.minHumidity.humidity > item.humidity) {
+            result.minHumidity = item;
+        }
+
+        if (result.maxTemperature.temperature < item.temperature) {
+            result.maxTemperature = item;
+        }
+
+        if (result.minTemperature.temperature > item.temperature) {
+            result.minTemperature = item;
+        }
+
+        if (index % 2 === 0) {
+            return result;
+        }
+
+        result.date.push(format(item.date, 'dd.MM.yy HH:mm'));
+        result.temperature.push(item.temperature);
+        result.humidity.push(item.humidity);
+
+        return result;
+    }, initData);
+};
+
+
+// function distributedCopy(items, n) {
+//     const result = [items[0]];
+//     const totalItems = items.length - 2;
+//     const interval = Math.floor(totalItems / (n - 2));
+//
+//     for (let i = 1; i < n - 1; i++) {
+//         result.push(items[i * interval]);
+//     }
+//
+//     result.push(items[items.length - 1]);
+//
+//     return result;
+// }
 
 module.exports = {
     getContext,
     getSensorDataByKey,
+    range,
+    reduceItemsCountBy,
+    processData,
 };
