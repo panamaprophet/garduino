7f1e66cc9c7179d0b235e4bf6898e453068ac72a
Sergey Klykov <pp@rawsound.ru>
2020-08-17 16:14:16 +0200

    a bit of refactoring
    rearranged controllers and actions

--- server/bot/controllers/now/index.js
+++ server/bot/controllers/now/index.js
@@ -1,30 +1,20 @@
 const WizardScene = require('telegraf/scenes/wizard');
+const {getInlineKeyboard} = require('../../helpers');
+const {getControllerIds} = require('../../../resolvers/controller');
+const {selectController} = require('../common');
 const {
     actionHandler,
     ACTION_NOW,
     ACTION_STAT_DAY,
     ACTION_STAT_WEEK,
-} = require('../actions');
-const {getInlineKeyboard} = require('../helpers');
-const {getControllerIds} = require('../../resolvers/controller');
+} = require('./actions');
 
 
 const SELECT_CONTROLLER_STEP_INDEX = 0;
 
 
-const selectController = async ctx => {
-    const {db, chat} = ctx;
-    const {id: chatId} = chat;
-    const controllerIds = await getControllerIds(db, {chatId});
-
-    ctx.reply('Select a controller', getInlineKeyboard(controllerIds));
-
-    return ctx.wizard.next();
-};
-
 const selectAction = async ctx => {
-    const {db, chat} = ctx;
-    const {id: chatId} = chat;
+    const {db, chat: {id: chatId}} = ctx;
     const selectedControllerId = ctx.update.callback_query.data;
     const controllerIds = await getControllerIds(db, {chatId});
 
@@ -40,8 +30,7 @@ const selectAction = async ctx => {
 };
 
 const handleAction = async ctx => {
-    const {db, chat} = ctx;
-    const {id: chatId} = chat;
+    const {db, chat: {id: chatId}} = ctx;
     const selectedAction = ctx.update.callback_query.data;
     const {controllerId} = ctx.session;
 
