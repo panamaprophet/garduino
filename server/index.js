03f868d3c32f88257d013beb52de2bcbdda9436e
sergey klykov <pp@rawsound.ru>
2020-08-25 21:12:25 +0200

    typescript just because

--- server/index.js
+++ server/index.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const {createBotInstance} = require('./bot');
-
-const config = require('./config');
-const app = express();
-const uri = `mongodb+srv://${config.db.user}:${config.db.pass}@${config.db.host}/${config.db.database}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-client.connect().then(client => {
-    const db = client.db();
-    const bot = createBotInstance(db, config.bot);
-
+"use strict";
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var createBotInstance = require('./bot').createBotInstance;
+var config = require('./config');
+var app = express();
+var uri = "mongodb+srv://" + config.db.user + ":" + config.db.pass + "@" + config.db.host + "/" + config.db.database + "?retryWrites=true&w=majority";
+var client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect().then(function (client) {
+    var db = client.db();
+    var bot = createBotInstance(db, config.bot);
     app.locals.db = db;
-
     app.use(express.json());
     app.use(bot.webhookCallback(config.bot.webHookPath));
     app.use('/api', require('./routes'));
-
     bot.launch();
-    app.listen(config.port, () => console.log(`server launched on :${config.port}`));
+    app.listen(config.port, function () { return console.log("server launched on :" + config.port); });
 });
