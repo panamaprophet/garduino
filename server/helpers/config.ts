48ee70d35cd80339bc4854c1bfc15d47e6a343db
pp <pp@rawsound.ru>
2022-01-12 02:16:31 +0100

    dirty time calculation hack. need to be revisited asap

--- server/helpers/config.ts
+++ server/helpers/config.ts
@@ -1,20 +1,24 @@
-import {parse, addMilliseconds, compareDesc, differenceInMilliseconds} from 'date-fns';
+import {parse, addMilliseconds, compareDesc, differenceInMilliseconds, subDays} from 'date-fns';
 import {identity} from 'ramda';
 import {ConfigEntity, ConfigEntityRaw, ControllerConfigRaw} from 'types';
 
 
+const getHoursFromTimeString = (time: string) => Number(time.split(':')[0]);
+
 export const getConfigEntity = ({duration, onTime: onTimeString}: ConfigEntityRaw): ConfigEntity => {
     const currentDate = Date.now();
-    const onTime = parse(onTimeString, 'HH:mm', currentDate);
+    const onHours = getHoursFromTimeString(onTimeString);
+    const durationHours = (duration / 1000 / 60 / 60);
+
+    const onTime = subDays(
+        parse(onTimeString, 'HH:mm', currentDate), 
+        onHours + durationHours > 24 ? 1 : 0
+    );
+
     const offTime = addMilliseconds(onTime, duration);
+
     const isOn = compareDesc(onTime, currentDate) >= 0 && compareDesc(offTime, currentDate) < 0;
     const msBeforeSwitch = differenceInMilliseconds(isOn ? offTime : onTime, currentDate);
-
-    console.log(onTime);
-    console.log(offTime);
-    console.log(isOn);
-    console.log(msBeforeSwitch);
-    console.log('===');
 
     return {
         isOn,
