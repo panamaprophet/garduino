e399d3c1cec3eadb030f66c48589d5d20f245dbb
pp <pp@rawsound.ru>
2022-09-10 10:02:42 +0200

    move removeFromCache to separate function

--- api/websocket/index.ts
+++ api/websocket/index.ts
@@ -7,18 +7,20 @@ export const getWebSocketServer = (server: Server): [WebSocketServer, Map<string
     const wss = new WebSocketServer({ server });
     const cache = new Map<string, WebSocket>();
 
+    const removeFromCache = (ws: WebSocket) => {
+        const cacheEntries = Array.from(cache.entries());
+        const cacheEntry = cacheEntries.find(item => item[1] === ws);
+
+        if (cacheEntry) {
+            cache.delete(cacheEntry[0]);
+            console.log('[ws] removed from cache', cacheEntry[0]);
+        }
+    };
+
     wss.on('connection', (ws) => {
         console.log('[ws] new connection');
 
-        ws.on('close', () => {
-            const cacheEntries = Array.from(cache.entries());
-            const cacheEntry = cacheEntries.find(item => item[1] === ws);
-
-            if (cacheEntry) {
-                console.log('[ws] removing from cache', cacheEntry[0]);
-                cache.delete(cacheEntry[0]);
-            }
-        });
+        ws.on('close', () => removeFromCache(ws));
 
         const cacheByControllerId = (arrayBuffer: ArrayBuffer) => {
             const message: unknown = JSON.parse(arrayBuffer.toString());
